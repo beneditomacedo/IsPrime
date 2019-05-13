3250da42ee8ba7384c48c6ac343efa9130f77da3
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2019-05-12 21:42:35 -0300

    running properly without auto layout

--- IsPrime/ViewController.swift
+++ IsPrime/ViewController.swift
@@ -8,11 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var inputNumber: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func checkNumber(_ sender: Any) {
+        if let number = inputNumber.text {
+            if let numberInt = Int(number) {
+                
+                let limit = Int(sqrt(Double(numberInt)) + 1)
+                var isPrime = true
+                    
+                for i in 2 ... Int(limit) {
+                    if numberInt % i == 0 {
+                        isPrime = false
+                        break
+                    }
+                }
+                
+                if isPrime {
+                    result.text = "O número \(numberInt) é primo"
+                } else {
+                    result.text = "O número \(numberInt) não é primo"
+                }
+                
+                inputNumber.text = ""
+            }
+        }
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.inputNumber.placeholder = nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.inputNumber.delegate = self
     }
 
 
